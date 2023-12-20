c4c1288e158bbe6acbee3dd22d45a99fac65cb6c
Yaroslav-Pa <yaroslav.roro@gmail.com>
2023-12-20 10:52:18 +0200

    redacted SR PR6

--- SR/LR6/3.py
+++ SR/LR6/3.py
@@ -1,3 +1,5 @@
+from variables3 import graph
+
 def floyd_warshall(graph):
     num_vertices = len(graph)
     vertices = list(graph.keys())
@@ -28,19 +30,6 @@ def construct_path(next_vertex_matrix, start, end):
         start = next_vertex_matrix[start][end]
     path.append(end)
     return path
-
-graph = {
-    'A': {'B': 1, 'C': 4, 'D': 2},
-    'B': {'A': 1, 'C': 2, 'D': 5, 'E': 3},
-    'C': {'A': 4, 'B': 2, 'D': 1, 'E': 7},
-    'D': {'A': 2, 'B': 5, 'C': 1, 'E': 6, 'F': 8},
-    'E': {'B': 3, 'C': 7, 'D': 6, 'F': 1, 'G': 5},
-    'F': {'D': 8, 'E': 1, 'G': 3, 'H': 9},
-    'G': {'E': 5, 'F': 3, 'H': 4, 'I': 2},
-    'H': {'F': 9, 'G': 4, 'I': 7, 'J': 6},
-    'I': {'G': 2, 'H': 7, 'J': 5},
-    'J': {'H': 6, 'I': 5}
-}
 
 shortest_distances, next_vertex_matrix = floyd_warshall(graph)
 
